19dd6a07ccf0bc1a34971e94f135c477e9257504
Henio <henioajunior@gmail.com>
2021-06-24 16:52:56 -0300

    Retornando o token para o cliente

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.dto.LoginForm;
+import br.com.alura.forum.dto.TokenDto;
 
 @RestController
 @RequestMapping("/auth")
@@ -28,13 +29,12 @@ public class AutenticacaoController {
 		
 	
 	@PostMapping
-	public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form) {
+	public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form) {
 		UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 		try {
 			Authentication authentication = authManager.authenticate(dadosLogin);
 			String token = tokenService.gerarToken(authentication);
-			System.out.println(token);
-			return ResponseEntity.ok().build();
+			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
 			
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().build();
