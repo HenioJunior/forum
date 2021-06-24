5bbcc2cb76210035931a94e2358b976c2181043e
Henio <henioajunior@gmail.com>
2021-06-24 16:42:13 -0300

    Gerando tokens com JWT

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,17 +1,17 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.header.writers.frameoptions.StaticAllowFromStrategy;
 
 @EnableWebSecurity
 @Configuration
@@ -19,6 +19,12 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	private AutenticacaoService autenticacaoService;
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -2,23 +2,43 @@ package br.com.alura.forum.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.dto.LoginForm;
 
 @RestController
 @RequestMapping("/auth")
 public class AutenticacaoController {
 	
+	@Autowired
+	private AuthenticationManager authManager;
+	
+	
+	@Autowired
+	private TokenService tokenService;
+		
+	
 	@PostMapping
 	public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form) {
-		System.out.println(form.getEmail());
-		System.out.println(form.getSenha());
+		UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+		try {
+			Authentication authentication = authManager.authenticate(dadosLogin);
+			String token = tokenService.gerarToken(authentication);
+			System.out.println(token);
+			return ResponseEntity.ok().build();
+			
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
 		
-		return ResponseEntity.ok().build();
 	}
 }
