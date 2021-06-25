3c424695bbe9f6ab6e6a81937032014397b5e332
Henio <henioajunior@gmail.com>
2021-06-25 17:34:32 -0300

    Testando um controller

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -1,17 +1,19 @@
 package br.com.alura.forum.repository;
 
+import br.com.alura.forum.modelo.Curso;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import br.com.alura.forum.modelo.Curso;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 	
 	@Autowired
